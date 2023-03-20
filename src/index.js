4c4de2ae42dd4a236267e19d6702885f1d603a9e
makeyzn <tntmr774@gmail.com>
2023-03-20 23:56:49 +0300

    dark mode

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+import { store } from './store/index';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
