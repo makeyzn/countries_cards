db57e9e6916bcafc453b466459afb3c917399e11
makeyzn <tntmr774@gmail.com>
2023-03-22 00:54:08 +0300

    select regions

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,14 +7,14 @@ import { Controls } from '../components/Controls';
 import { selectCountries, selectVisibleCountries } from '../store/countries/countries-selectors';
 import { useEffect } from 'react';
 import { loadCountries } from '../store/countries/countries-actions';
-import { selectSearch } from '../store/controls/controls-selectors';
+import { selectControls, selectSearch } from '../store/controls/controls-selectors';
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
-  const search = useSelector(selectSearch);
-  const countries = useSelector( state => selectVisibleCountries(state, {search}));
+  const {search, region} = useSelector(selectControls);
+  const countries = useSelector( state => selectVisibleCountries(state, {search, region}));
   const {status, error, qty} = useSelector(selectCountries);
 
   useEffect(() => {
