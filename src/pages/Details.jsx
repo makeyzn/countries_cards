4e04d193baa469f65b79a5e1b645fed2aa0c9aca
makeyzn <tntmr774@gmail.com>
2023-03-24 00:29:13 +0300

    Datails page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,19 +3,33 @@ import { IoArrowBack } from 'react-icons/io5';
 
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentCountry, selectDetails } from '../store/details/details-selector';
+import { useEffect } from 'react';
+import { clearDetails, loadCountryByName } from '../store/details/details-action';
 
 
 export const Details = () => {
   const { name } = useParams();
+  const dispatch = useDispatch();
+  const {currentCountry, error, status} = useSelector(selectDetails)
   const navigate = useNavigate();
 
-  const currentCountry = null;
+  useEffect(() => {
+    dispatch(loadCountryByName(name));
+
+    return () => {
+      dispatch(clearDetails());
+    }
+  }, [name, dispatch])
 
   return (
     <div>
       <Button onClick={() => navigate(-1)}>
         <IoArrowBack /> Back
       </Button>
+      {status === 'loading' && <h2>Loading...</h2>}
+      {status === 'error' && <h2>{error}</h2>}
       {currentCountry && <Info push={navigate} {...currentCountry} />}
     </div>
   );
